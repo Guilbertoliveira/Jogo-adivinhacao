905cfa1d450d37e825991d406aa2e15410e1d7a6
Guilbert <guilbertoliveira@hotmail.com>
2018-08-28 20:17:55 -0300

    Jogo da Forca

---
+++ Forca.py
@@ -0,0 +1,9 @@
+
+def jogar(): #definindo uma função para py
+    print("*****************************")
+    print("Bem vindo no jogo de Forca")
+    print("*****************************")
+
+    print("Fim de Jogo")
+
+
