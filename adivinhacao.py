d5482ba350eb1aeeecefb5817605d2588b5995f8
Guilbert <guilbertoliveira@hotmail.com>
2018-08-28 20:29:50 -0300

    Versao Atualizada

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,62 +1,63 @@
 import random # modulo que necessita ser adicionado pelo random
 
 
+def jogar(): #criando função do jogo adivinhação
 
-print("***********************")
-print("Bem vindo no jogo de advinhação")
-print("*****************************")
-
-
-# numero_secreto = round(random.random()* 100) #pegando um numero aleatorio e arredondando ele para um int.
-numero_secreto = random.randrange(0,51) # com esse conseguimos limitar de 0 a 50 o numero secreto
+    print("***********************")
+    print("Bem vindo no jogo de advinhação")
+    print("*****************************")
 
 
-total_tentativas = 0
-pontos = 1000
+    # numero_secreto = round(random.random()* 100) #pegando um numero aleatorio e arredondando ele para um int.
+    numero_secreto = random.randrange(0,51) # com esse conseguimos limitar de 0 a 50 o numero secreto
 
-print("qual numero de dificuldade deseja ?")
-print("Facil : 1, Medio  : 2, Dificil: 3 ")
 
-nivel = int(input("defina o nivel \n")) # pegando dados e passando para variavel nivel, porem antes convertendo para int
+    total_tentativas = 0
+    pontos = 1000
 
-if (nivel==1):
-    total_tentativas = 15
-elif (nivel==2):
-    total_tentativas = 10
-else:
-    total_tentativas = 5
+    print("qual numero de dificuldade deseja ?")
+    print("Facil : 1, Medio  : 2, Dificil: 3 ")
 
-# print(numero_secreto) testando resultado
+    nivel = int(input("defina o nivel \n")) # pegando dados e passando para variavel nivel, porem antes convertendo para int
 
-for rodada in range(1, total_tentativas+1):
-
-    print("tentativa {} de {}".format(rodada, total_tentativas))
-
-    chute = input("digite o seu numero entre 1 e 50 \n")
-    chuteconvertido = int(chute)
- 
-    if (chuteconvertido < 1 or chuteconvertido > 50):
-        print("digite numeros entre 1 e 50 ! \n")
-        continue #continue passa uma vez do laço porem continua nele, diferente do break que sai do laço.
-    print("voce digitou \t", chute)
-
-    acertou =   chuteconvertido == numero_secreto
-    maior=   chuteconvertido > numero_secreto
-    menor=  chuteconvertido < numero_secreto
-
-    if(acertou):
-        print("voce acertou e fez {} pontos".format(pontos) )
-        break
-
+    if (nivel==1):
+        total_tentativas = 15
+    elif (nivel==2):
+        total_tentativas = 10
     else:
-        if(maior):
-            print("voce errou! o seu numero foi maior que o numero secreto \n")
-        elif(menor):
-            print("voce errou! o seu numero foi menor que o numero secreto \n")
+        total_tentativas = 5
 
-            pontos_perdidos = abs(numero_secreto - chuteconvertido) # numero aleatorio - valordigitadopelousuario com o abs o valor se torna absoluro (sempre positivo)
-            pontos = pontos - pontos_perdidos
+    # print(numero_secreto) testando resultado
+
+    for rodada in range(1, total_tentativas+1):
+
+        print("tentativa {} de {}".format(rodada, total_tentativas))
+
+        chute = input("digite o seu numero entre 1 e 50 \n")
+        chuteconvertido = int(chute)
+
+        if (chuteconvertido < 1 or chuteconvertido > 50):
+            print("digite numeros entre 1 e 50 ! \n")
+            continue #continue passa uma vez do laço porem continua nele, diferente do break que sai do laço.
+        print("voce digitou \t", chute)
+
+        acertou =   chuteconvertido == numero_secreto
+        maior=   chuteconvertido > numero_secreto
+        menor=  chuteconvertido < numero_secreto
+
+        if(acertou):
+            print("voce acertou e fez {} pontos".format(pontos) )
+            break
+
+        else:
+            if(maior):
+                print("voce errou! o seu numero foi maior que o numero secreto \n")
+            elif(menor):
+                print("voce errou! o seu numero foi menor que o numero secreto \n")
+
+                pontos_perdidos = abs(numero_secreto - chuteconvertido) # numero aleatorio - valordigitadopelousuario com o abs o valor se torna absoluro (sempre positivo)
+                pontos = pontos - pontos_perdidos
 
 
 
-print("Fim")
+    print("Fim")
