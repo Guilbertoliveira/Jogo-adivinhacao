2b16263a60b55d05f2e2e36c33b17023ff7fc946
Guilbert <guilbertoliveira@hotmail.com>
2018-08-28 18:45:46 -0300

    Nova versão

--- adivinhacao.py
+++ adivinhacao.py
@@ -11,20 +11,33 @@ print("*****************************")
 numero_secreto = random.randrange(0,51) # com esse conseguimos limitar de 0 a 50 o numero secreto
 
 
-total_tentativas = 3
+total_tentativas = 0
+pontos = 1000
+
+print("qual numero de dificuldade deseja ?")
+print("Facil : 1, Medio  : 2, Dificil: 3")
+
+nivel = int(input("defina o nivel")) # pegando dados e passando para variavel nivel, porem antes convertendo para int
+
+if (nivel==1):
+    total_tentativas = 15
+elif (nivel==2):
+    total_tentativas = 10
+else:
+    total_tentativas = 5
 
 # print(numero_secreto) testando resultado
 
 for rodada in range(1, total_tentativas+1):
 
     print("tentativa {} de {}".format(rodada, total_tentativas))
+
     chute = input("digite o seu numero entre 1 e 50")
     chuteconvertido = int(chute)
  
     if (chuteconvertido < 1 or chuteconvertido > 50):
         print("digite numero entre 1 e 50")
-        continue
-
+        continue #continue passa uma vez do laço porem continua nele, diferente do break que sai do laço.
     print("voce digitou", chute)
 
     acertou =   chuteconvertido == numero_secreto
@@ -32,7 +45,7 @@ for rodada in range(1, total_tentativas+1):
     menor=  chuteconvertido < numero_secreto
 
     if(acertou):
-        print("voce acertou")
+        print("voce acertou e fez {} pontos".format(pontos) )
         break
 
     else:
@@ -40,6 +53,9 @@ for rodada in range(1, total_tentativas+1):
             print("voce errou! o seu numero foi maior que o numero secreto")
         elif(menor):
             print("voce errou! o seu numero foi menor que o numero secreto")
+
+            pontos_perdidos = abs(numero_secreto - chuteconvertido) # numero aleatorio - valordigitadopelousuario com o abs o valor se torna absoluro (sempre positivo)
+            pontos = pontos - pontos_perdidos
 
 
 
